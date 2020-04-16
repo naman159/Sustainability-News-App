fcb3366af428c80eebf0f1d3a65348fb865763e7
naman159 <namanranawat159@gmail.com>
2020-04-17 02:28:04 +0530

    Updates

---
+++ precache-manifest.d228e43542077d433bb4405cf7fb866b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50de70f75048e3eb0291712e27303cbe",
+    "url": "/sust-news-app/index.html"
+  },
+  {
+    "revision": "bc7888b41226097a13ba",
+    "url": "/sust-news-app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "81d77c81914203cf7b85",
+    "url": "/sust-news-app/static/js/2.3effefb7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sust-news-app/static/js/2.3effefb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc7888b41226097a13ba",
+    "url": "/sust-news-app/static/js/main.09b962f0.chunk.js"
+  },
+  {
+    "revision": "a1aca71e8e29d722601c",
+    "url": "/sust-news-app/static/js/runtime-main.88435505.js"
+  }
+]);
