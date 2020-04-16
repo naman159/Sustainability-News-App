1b3f62e48b223205abe5dea6e43417578e996a9f
naman159 <namanranawat159@gmail.com>
2020-04-17 01:46:11 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      articles: []
+    };
+  }
+
+  componentWillMount() {
+    var url = 'http://newsapi.org/v2/everything?' +
+          'q=Sustainability&' +
+          'sortBy=popularity&' +
+          'apiKey=8cf61293b6c6493a875aa43562e078bb';
+
+    /*var req = new Request(url);
+    fetch(req)
+        .then(response => response.json())
+        .then(articles => console.log({articles}));*/
+        axios.get(url)
+        .then(res => {
+          // Set state with result
+          this.setState({ articles: res.data.articles });
+          console.log(this.state.articles[0]);
+        });
+  }
+
+  render() {
+    return (
+      <div className="App">
+          <h1>Latest on Sustainability</h1>
+        {this.state.articles.map((article) => (
+          <a href={article.url}>
+            <div className="ba pa3 ma3">
+              <h3>{article.title}</h3>
+              <p>{article.content} -{article.author}</p>
+            </div>
+          </a>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
